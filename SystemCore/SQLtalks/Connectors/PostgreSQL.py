ddabc2e5ae83f2467a8d9fe135c199ef87a77a49
Vladimir Miller <miller.wm.fp@gmail.com>
2020-03-05 08:03:31 +0700

    DTO для подключения вынесен отдельно

--- SystemCore/SQLtalks/Connectors/PostgreSQL.py
+++ SystemCore/SQLtalks/Connectors/PostgreSQL.py
@@ -1,89 +1,12 @@
+'''
+Модуль для общения с PostgreSQL
 
+'''
 
 import psycopg2
-import time
 
+from SystemCore.SQLtalks.Connectors.ConnectionDTOs import ConnectionData
 
-class ConnectionData:
-    '''
-    Объект, хранящий данные для подключения
-    '''
-    def __init__(self, base_name: str,
-                 host: str, port: str,
-                 user: str, password: str):
-
-        self.__base_name = base_name
-        self.__host = host
-        self.__port = str(port)
-        self.__user = user
-        self.__password = password
-
-    # ------------------------------------------------------------------------------------------------
-    # Доступы ----------------------------------------------------------------------------------------
-    # ------------------------------------------------------------------------------------------------
-    @property
-    def base_name(self) -> str:
-        '''
-        Имя базы
-
-        :return:
-        '''
-        return self.__base_name
-
-    @property
-    def host(self) -> str:
-        '''
-        Хост
-
-        :return:
-        '''
-        return self.__host
-
-    @property
-    def port(self) -> str:
-        '''
-        Порт в виде строки
-
-        :return:
-        '''
-        return self.__port
-
-    @property
-    def user(self) -> str:
-        '''
-        имя пользователя
-
-        :return:
-        '''
-        return self.__user
-
-    @property
-    def password(self) -> str:
-        '''
-        пароль
-
-        :return:
-        '''
-        return self.__password
-
-
-    @property
-    def server(self) -> str:
-        '''
-        Отдаёт хост и порт
-
-        :return: "хост:порт"
-        '''
-        return f'{self.host}:{self.port}'
-
-    @property
-    def authorization(self) -> str:
-        '''
-        Отдаёт пользователя и пароль
-
-        :return: "пользователь:пароль"
-        '''
-        return f'{self.user}:{self.password}'
 
 # ------------------------------------------------------------------------------------------------
 # Выполнение запросов ----------------------------------------------------------------------------
