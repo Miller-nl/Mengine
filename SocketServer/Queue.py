838f3c768fae4699cf335938512e36be48e42d24
Miller_nl <miller.pgm.ww@gmail.com>
2020-06-04 16:21:02 +0700

    Комментарии по работе сокета

--- SocketServer/Queue.py
+++ SocketServer/Queue.py
@@ -10,3 +10,20 @@
 
 
 
+'''
+def g(message):
+    print(message)
+    return
+arg = {'message': 'ВСЁ РАБОТАЕТ!'}
+g(**arg)
+# ВСЁ РАБОТАЕТ!
+
+def f(message, **kwargs):
+    print(message)
+    print(kwargs)
+    return
+new_arg = {'arg_1': 'да да да!!'}
+f('Сообщение', **new_arg)
+#Сообщение
+#{'arg_1': 'да да да!!'}
+'''
