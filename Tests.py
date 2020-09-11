4c0715f35815e440dca619be6a3f152f26d4d620
Miller_nl <miller.pgm.ww@gmail.com>
2020-09-11 21:34:22 +0700

    Подчищено лишнее

--- Tests.py
+++
@@ -1,9 +0,0 @@
-
-
-from Logging import CommonLoggingClient
-from Logging.LoggingWorkers import JsonLogger
-
-JL = JsonLogger('D:\Projects\Tests')
-CLC = CommonLoggingClient('Тест 1')
-CLC.add_writer(JL, 'JL')
-CLC.to_log('Mes 1')
